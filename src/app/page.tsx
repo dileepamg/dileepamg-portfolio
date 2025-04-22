d3a3d03340a110f1c839f2ca943c218ce5d69944
Dileepa Galmangoda <t2mahanama@gmail.com>
2025-04-22 16:57:20 -0700

    Updated the UI component library and content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,122 @@
-import Image from "next/image";
-import React from "react";
-import { CardBody, CardContainer, CardItem } from "../components/ui/3d-card";
-
+"use client";
+import React, { useState } from "react";
+import { cn } from "@/lib/utils";
+import { TracingBeam } from "../components/ui/tracing-beam";
+import { CalendarDays } from "lucide-react";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <CardContainer className="inter-var">
-          <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
-            <CardItem
-              translateZ="50"
-              className="text-xl font-bold text-neutral-600 dark:text-white"
-            >
-              Make things float in air
-            </CardItem>
-            <CardItem
-              as="p"
-              translateZ="60"
-              className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-            >
-              Hover over this card to unleash the power of CSS perspective
-            </CardItem>
-            <CardItem translateZ="100" rotateX={20} className="w-full mt-4">
-              <Image
-                src="/room.jpg"
-                height="1000"
-                width="1000"
-                className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                alt="thumbnail"
-              />
-            </CardItem>
-            <div className="flex justify-between items-center mt-20">
-              <CardItem
-                translateZ={20}
-                translateX={-40}
-                as="button"
-                className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-              >
-                Try now →
-              </CardItem>
-              <CardItem
-                translateZ={20}
-                translateX={40}
-                as="button"
-                className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-              >
-                Sign up
-              </CardItem>
-            </div>
-          </CardBody>
-        </CardContainer>
-      </main>
+    <div className="inset-0 flex h-screen items-center justify-center">
+      <div
+        className={cn(
+          "fixed inset-0",
+          "[background-size:20px_20px]",
+          "[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
+          "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]",
+        )}
+      />
+      <div className="relative m-8 mt-6 max-h-3/4 w-fit max-w-4xl rounded-3xl p-2 md:rounded-3xl md:p-2 dark:bg-black">
+        <TracingBeam className="h-screen px-6">
+          <div className="relative mx-auto max-w-2xl p-4 antialiased">
+            {dummyContent.map((item, index) => (
+              <div key={`content-${index}`} className="mb-10">
+                <h2 className="mb-4 flex w-fit items-center space-x-2 rounded-full bg-black py-1 text-sm text-white">
+                  <div>
+                    <CalendarDays size={18} color="#4285F4" />
+                  </div>
+                  <div>{item.badge}</div>
+                </h2>
+
+                <p className="mb-4 text-xl">{item.title}</p>
+
+                <div className="prose prose-sm dark:prose-invert text-sm">
+                  {item.description}
+                </div>
+              </div>
+            ))}
+          </div>
+        </TracingBeam>
+      </div>
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="pointer-events-none fixed inset-0 flex h-full items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_50%,black)] dark:bg-black"></div>
     </div>
   );
 }
+
+interface GridItemProps {
+  area: string;
+  // icon: React.ReactNode;
+  title: string;
+  description: React.ReactNode;
+}
+
+const dummyContent = [
+  {
+    title: "Senior UI/UX Designer",
+    description: (
+      <>
+        <p>
+          Sit duis est minim proident non nisi velit non consectetur. Esse
+          adipisicing laboris consectetur enim ipsum reprehenderit eu deserunt
+          Lorem ut aliqua anim do. Duis cupidatat qui irure cupidatat incididunt
+          incididunt enim magna id est qui sunt fugiat. Laboris do duis pariatur
+          fugiat Lorem aute sit ullamco. Qui deserunt non reprehenderit dolore
+          nisi velit exercitation Lorem qui do enim culpa. Aliqua eiusmod in
+          occaecat reprehenderit laborum nostrud fugiat voluptate do Lorem culpa
+          officia sint labore. Tempor consectetur excepteur ut fugiat veniam
+          commodo et labore dolore commodo pariatur.
+        </p>
+        <p>
+          Dolor minim irure ut Lorem proident. Ipsum do pariatur est ad ad
+          veniam in commodo id reprehenderit adipisicing. Proident duis
+          exercitation ad quis ex cupidatat cupidatat occaecat adipisicing.
+        </p>
+        <p>
+          Tempor quis dolor veniam quis dolor. Sit reprehenderit eiusmod
+          reprehenderit deserunt amet laborum consequat adipisicing officia qui
+          irure id sint adipisicing. Adipisicing fugiat aliqua nulla nostrud.
+          Amet culpa officia aliquip deserunt veniam deserunt officia
+          adipisicing aliquip proident officia sunt.
+        </p>
+      </>
+    ),
+    badge: "April 2025 to Present",
+  },
+  {
+    title: "Lorem Ipsum Dolor Sit Amet",
+    description: (
+      <>
+        <p>
+          Ex irure dolore veniam ex velit non aute nisi labore ipsum occaecat
+          deserunt cupidatat aute. Enim cillum dolor et nulla sunt exercitation
+          non voluptate qui aliquip esse tempor. Ullamco ut sunt consectetur
+          sint qui qui do do qui do. Labore laborum culpa magna reprehenderit ea
+          velit id esse adipisicing deserunt amet dolore. Ipsum occaecat veniam
+          commodo proident aliqua id ad deserunt dolor aliquip duis veniam sunt.
+        </p>
+        <p>
+          In dolore veniam excepteur eu est et sunt velit. Ipsum sint esse
+          veniam fugiat esse qui sint ad sunt reprehenderit do qui proident
+          reprehenderit. Laborum exercitation aliqua reprehenderit ea sint
+          cillum ut mollit.
+        </p>
+      </>
+    ),
+    badge: "Changelog",
+  },
+  {
+    title: "Lorem Ipsum Dolor Sit Amet",
+    description: (
+      <>
+        <p>
+          Ex irure dolore veniam ex velit non aute nisi labore ipsum occaecat
+          deserunt cupidatat aute. Enim cillum dolor et nulla sunt exercitation
+          non voluptate qui aliquip esse tempor. Ullamco ut sunt consectetur
+          sint qui qui do do qui do. Labore laborum culpa magna reprehenderit ea
+          velit id esse adipisicing deserunt amet dolore. Ipsum occaecat veniam
+          commodo proident aliqua id ad deserunt dolor aliquip duis veniam sunt.
+        </p>
+      </>
+    ),
+    badge: "Launch Week",
+  },
+];
